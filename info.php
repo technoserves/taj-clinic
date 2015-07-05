6baef77875434e6fa53db22929c02903106c16dc
Sheryar Shirazi <sheryarshirazi@gmail.com>
2015-07-06 00:48:33 +0500

    add initial files

---
+++ info.php
@@ -0,0 +1,4 @@
+<form action='in.php'>
+<input name='name'>
+<input type='submit'>
+</form>
---
+++ js/custom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#show-panel").click(function(){$("#lightbox, #lightbox-panel").fadeIn(300)});$("a#close-panel").click(function(){$("#lightbox, #lightbox-panel").fadeOut(300)})})
